76cd6ccc0863fb8f7750060155fc47d32cf8f644
Charles Collett <cacollet@hamilton.edu>
2024-03-06 09:50:10 -0600

    Removing general files and putting in ESR files

--- pyscan/drivers/__init__.py
+++ pyscan/drivers/__init__.py
@@ -1,80 +1,54 @@
 # Objects
-from .instrument_driver import InstrumentDriver
-
+from .instrumentdriver import InstrumentDriver
+from .driver import Driver
 # Instrument Drivers
-from .agilent33500 import Agilent33500
-from .agilent34410 import Agilent34410
-from .agilentdso900series import AgilentDSO900Series
-from .agilent8267d import AgilentE8267D
-from .agilent8275n import Agilent8275N
-from .americanmagnetics430 import AmericanMagnetics430
-from .blueforslog import BlueForsLog
-from .bkprecision9130b import BKPrecision9130B
-from .hp34401a import HP34401A
-from .keithley2400 import Keithley2400
-from .kepcoBOP import KepcoBOP
-from .keysitem3302adaq import KeysiteM3302ADAQ
-from .keysitem3302aawg import KeysiteM3302AAWG
-from .keysite53230a import Keysite53230A
-from .oxfordips120 import OxfordIPS120
-from .pulselaser import PulseLaser
-from .stanford396 import Stanford396
-from .stanford400 import Stanford400
-from .stanford470 import Stanford470
-from .stanford620 import Stanford620
-from .stanford830 import Stanford830
-from .stanford860 import Stanford860
-from .stanford900 import Stanford900
-from .stanford928 import Stanford928
-from .tpi1002a import TPI1002A
-from .yokogawags200 import YokogawaGS200
-from .actonsp2300 import ActonSP2300
-
 try:
-    from .attocubeANC350 import AttocubeANC350
-except ModuleNotFoundError:
-    print('pylablib not found, AttocubeANC350 not loaded')
-
+    from .oscilloscope import Oscilloscope
+except OSError:
+    print('Oscilloscope not found, generic driver not loaded')
 try:
-    from .baslercamera import BaslerCamera
-except ModuleNotFoundError:
-    print('Basler Camera software not found, BaserCamera not loaded')
-
+    from .bkprecision2190e import BKPrecision2190E
+except OSError:
+    print('BK2190E Oscilloscope not found, BKPrecision2190E not loaded')
 try:
-    from .helioscamera import HeliosCamera
-
-except ModuleNotFoundError:
-    print('Helios Camera not installed')
-
+    from .tektronix2022b import Tektronix2022B
+except OSError:
+    print('2022B Oscilloscope not found, Tektronix2022B not loaded')
 try:
-    from .thorlabsbsc203 import BSC203
-except ModuleNotFoundError:
-    print('msl not installed, Thorlabs BSC203 driver not loaded')
-
+    from .tektronix1052b import Tektronix1052B
+except OSError:
+    print('1052B Oscilloscope not found, Tektronix1052B not loaded')
 try:
-    from .oceanopticsqepro import OceanOpticsQEPro
-except ModuleNotFoundError:
-    print('seabreeze module not found, Ocean Optics not imported')
+    from .tektronixmso2 import TektronixMSO2
+except OSError:
+    print('MSO2 Oscilloscope not found, TektronixMSO2 not loaded')
 try:
-    from .pulseblaster import PulseBlaster
-    from .pulseblasternv import NVPulseBlaster
-except NameError:
-    print('spinapi is not installed, PulseBlaster driver not loaded.')
+    from .ice40hx8k import ice40HX8K
+except OSError:
+    print('iceBoard not found, ice40HX8K not loaded')
 try:
-    from .thorlabsbsc203 import ThorlabsBSC203
-except ModuleNotFoundError:
-    print('Thorlabs Kinesis not found, ThorlabsBSC203 not loaded')
+    from .ecp5evn import ecp5evn
+except OSError:
+    print('ECP5 board not found, ecp5evn not loaded')
 try:
-    from .thorlabsbpc303 import ThorlabsBPC303
-except ModuleNotFoundError:
-    print('Thorlabs Kinesis not found, ThorlabsBPC303 not loaded')
+    from .windfreaksynthhd import WindfreakSynthHD
+except OSError:
+    print('SynthHD not found, WindFreakSynthHD not loaded')
 try:
-    from .thorlabsmff101 import ThorlabsMFF101
-except ModuleNotFoundError:
-    print('Thorlabs Kinesis not found, ThorlabsMFF101 not loaded')
+    from .windfreaksynthusbii import WindfreakSynthUSBii
+except OSError:
+    print('SynthUSBii not found, WindFreakSynthUSBii not loaded')
+try:
+    from .lakeshore335 import Lakeshore335
+except OSError:
+    print('Temperature Controller not found, Lakeshore335 not loaded')
+try:
+    from .gpd3303s import GPD3303S
+except OSError:
+    print('Power Supply not found, GPD3303S not loaded')
 
 # Methods
-from .new_instrument import new_instrument
+from .newinstrument import new_instrument
 
 # Test Devices
-from .test_voltage import TestVoltage
+
