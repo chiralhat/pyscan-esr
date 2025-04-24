a7ffe003817ae68aeab2884068e8e6de0bb611d3
root <root@pynq2.hamilton.edu>
2025-04-24 17:21:16 +0000

    graph updates only if data has changed, and plots real data

--- esr_notebooks/new_gui.py
+++ esr_notebooks/new_gui.py
@@ -341,18 +341,54 @@ class Worker(QObject):
         
 
         self.updateStatus.emit("Sweeping...\n")
-        while self.stop_requested == False and self.running == True:
-            if self.experiment.sweep['expt'].runinfo.running == False:
+        # Add these to your class __init__ or before the loop starts:
+        self.last_data = None
+
+        # Inside your thread's run loop:
+        while not self.stop_requested and self.running:
+            expt = self.experiment.sweep['expt']
+
+            if not expt.runinfo.running:
                 self.running = False
+
             elif expt.runinfo.measured != []:
-                
-                pg = ps.PlotGenerator(expt=expt,
-                                        d=2,
-                                        x_name='t',
-                                        y_name=self.experiment.parameters['y_name'],
-                                        transpose=1)
-                self.live_plot_update_signal.emit(pg)                     
-            sleep(1)  
+                try:
+                    current_data = expt.x  # Change 'x' to whatever you're interested in
+
+                    if self.last_data is None or not np.array_equal(current_data, self.last_data):
+                        self.last_data = current_data.copy()
+
+                        pg = ps.PlotGenerator(
+                            expt=expt,
+                            d=2,
+                            x_name='t',
+                            y_name=self.experiment.parameters['y_name'],
+                            data_name='x',
+                            transpose=1
+                        )
+
+                        self.live_plot_update_signal.emit(pg)
+
+                except Exception as e:
+                    print("Error in update loop:", e)
+                    
+            sleep(1)  # optional; reduce or remove if not needed
+
+        print(dir(expt))  # List all available methods and attributes
+        # measured = self.experiment.sweep['expt'].runinfo.measured
+        # print(type(self.experiment.sweep['expt'].runinfo.measured))
+        # for i, m in enumerate(measured[:10]):  # Show first 10 items
+        #     print(f"[{i}] {m}")
+        # pg = ps.PlotGenerator(
+        #                 expt=expt,
+        #                 d=2,
+        #                 x_name='t',
+        #                 y_name=self.experiment.parameters['y_name'],
+        #                 data_name='x',
+        #                 transpose=1
+        #             )
+
+        self.live_plot_update_signal.emit(pg)   
 
         if self.stop_requested:
             self.updateStatus.emit("Stop request detected. Exiting sweep early.\n")
