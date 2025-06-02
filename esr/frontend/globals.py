b9dede2003237ac9e9d9dbbb2cefdc5264e8159c
Charles Collett <cacollet@hamilton.edu>
2025-06-02 14:30:39 -0400

    Switch to main RFSoC

--- esr/frontend/globals.py
+++ esr/frontend/globals.py
@@ -7,4 +7,4 @@ Current Use:
 - Stores the backend server address for HTTP requests made by GUI and Worker modules.
 """
 
-server_address = "http://150.209.47.102:5000"
+server_address = "http://pynq.hamilton.edu:5000"
