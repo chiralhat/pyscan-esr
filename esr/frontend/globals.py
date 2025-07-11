abe90f673df059a8bfdc57085cfc9793da9287e5
Charles Collett <cacollet@hamilton.edu>
2025-07-11 15:25:50 -0400

    Add frontend from rfsoc branch (not yet changed)

---
+++ esr/frontend/globals.py
@@ -0,0 +1,10 @@
+"""
+globals.py
+
+Holds global constants and settings shared across the project.
+
+Current Use:
+- Stores the backend server address for HTTP requests made by GUI and Worker modules.
+"""
+
+server_address = "http://pynq.hamilton.edu:5000"
