3f71b54e98411d5607b7c80182c122f4bb584909
Charles Collett <cacollet@hamilton.edu>
2026-06-26 10:35:45 -0400

    Set p2start to 0

--- esr_notebooks/spinecho_gui.py
+++ esr_notebooks/spinecho_gui.py
@@ -96,6 +96,7 @@ def single_shot(sig, parameters, devices, output, fig):
 
 def init_experiment(devices, parameters, sweep):
     parameters['pulse2'] = parameters['pulse1']*parameters['mult']
+    parameters['p2start'] = 0
     # nut_time = parameters['nutation_width']+parameters['nutation_delay']
     # p1_time = parameters['pulse1']+(parameters['delay']*2+parameters['pulse2'])*parameters['cpmg']
     # switch_time = 500 + nut_time + p1_time
