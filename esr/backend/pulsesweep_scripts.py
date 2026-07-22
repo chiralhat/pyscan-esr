abbe8d2d306b8e1a9b5f1b038088475cfaf600d0
Charles Collett <cacollet@hamilton.edu>
2026-07-21 22:28:09 -0400

    Fix fit parameter saving

--- esr/backend/pulsesweep_scripts.py
+++ esr/backend/pulsesweep_scripts.py
@@ -11,8 +11,9 @@ def pf_single_shot(sig, parameters, devices):
     devices.scope.read_vxy(d=sig)
     freq = devices.synth.c_freqs
     fit, err = ps.exp_fit_norange(np.array([sig.time*1e6, sig.x]),
-                                       freq, 1)
+                                       freq, 1)[:2]
     sig.fit = fit
+    sig.err = err
 
 
 def read_wait(devices, parameters):
